073cc71c3e9e2fd7c4ff5be061bae983f505de4d
RonyPijuy7 <ronyhernandez7diciembre@gmail.com>
2024-03-05 20:01:39 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const navtoggle=document.querySelector(".toggle")
+const navmenu=document.querySelector(".nav-menu");
+navtoggle.addEventListener("click",()=>{ navmenu.classlist.toggle("nav-menu");});
